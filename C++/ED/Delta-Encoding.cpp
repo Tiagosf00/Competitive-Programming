8a2cbd66fee87d0118129946703e6a2a5147b841
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2019-07-29 20:51:13 -0300

    Delta encoding

---
+++ C++/ED/Delta-Encoding.cpp
@@ -0,0 +1,17 @@
+// Delta encoding
+
+	for(int i=0;i<q;i++) // Armazenando as mudanças no vetor delta
+	{
+	    int l,r,x;
+	    cin >> l >> r >> x; // Adicionar x de l até r
+	    delta[l] += x;
+	    delta[r+1] -= x;
+	}
+
+	int atual = 0;
+
+	for(int i=0;i<n;i++) // Atualizando o vetor
+	{
+	    atual += delta[i];
+	    v[i] += atual;
+	}
