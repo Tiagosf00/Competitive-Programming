b54264e4e3d87a9480fc6d7bfa4b87c3de68ce7d
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2020-01-25 07:05:38 -0300

    bfs

--- C++/Grafos/BFS.cpp
+++ C++/Grafos/BFS.cpp
@@ -5,11 +5,10 @@ int n; // number of nodes
 int s; // source vertex
 
 queue<int> q;
-vector<int> d(n, INF), p(n);
+vector<int> d(n, INF);
 
 q.push(s);
 used[s] = true;
-p[s] = -1;
 while (!q.empty()) {
     int v = q.front();
     q.pop();
@@ -17,19 +16,6 @@ while (!q.empty()) {
         if (d[u] > d[v] + 1) {
             q.push(u);
             d[u] = d[v] + 1;
-            //p[u] = v;
         }
     }
-}
-// Restore Path
-if (!used[u]) {
-    cout << "No path!";
-} else {
-    vector<int> path;
-    for (int v = u; v != -1; v = p[v])
-        path.push_back(v);
-    reverse(path.begin(), path.end());
-    cout << "Path: ";
-    for (int v : path)
-        cout << v << " ";
 }
