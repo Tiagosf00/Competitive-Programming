0a064a2db31df45980df256d2cee73a2682188d5
Alberto Duarte <albertotdneto@hotmail.com>
2020-02-23 19:29:06 -0300

    add mo's algorithm

---
+++ C++/ED/Mo.cpp
@@ -0,0 +1,37 @@
+struct Query {
+  int l, r, idx;
+  bool operator<(Query other) const
+  {
+    return make_pair(l / BLK, r) <
+    make_pair(other.l / BLK, other.r);
+  }
+};
+
+void add(); void remove() // implementar operações de acordo com o problema
+
+vector<pair<int,ll>> mo() {
+  vector<pair<int,ll>> res;
+  sort(queries.begin(), queries.end());
+
+  int l = 0, r = -1;
+  for(Query q : queries) {
+       while(l > q.l) {
+           l--;
+           add(l);
+       }
+       while(r < q.r) {
+           r++;
+           add(r);
+       }
+       while(l < q.l) {
+           remove(l);
+           l++;
+       }
+       while(r > q.r) {
+           remove(r);
+           r--;
+       }
+       res.pb(mp(q.idx, RESPOSTA)); // adicionar resposta de acordo com o problema
+   }
+   return res; // ordernar o vetor pelo indice e responder queries na ordem
+}
