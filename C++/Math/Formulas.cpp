bc473123e89be5cf53acf717349bc58915e618e4
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2020-02-26 02:32:03 -0300

    adicionando textos em tex no notebook

--- C++/Math/Formulas.cpp
+++
@@ -1,4 +0,0 @@
-int sum_x2(ll N)
-{
-	return (2*N*N*N + 3*N*N + N)/6;
-}
--- notebookcreator.py
+++ notebookcreator.py
@@ -7,26 +7,35 @@ def cpy_template():
 
 
 def get_dir():
-    items = os.listdir('C++/')
+    path = 'C++'
+    items = os.listdir(path)
     section = []
     for itemname in items:
         subsection = []
-        files = os.listdir('C++/'+itemname+'/')
+        files = os.listdir(os.path.join(path, itemname))
         for filename in files:
-            if filename.endswith('.cpp'):
+            if filename.endswith('.cpp') or filename.endswith('.tex'):
                 subsection.append(filename)
         section.append((itemname, subsection))
     return section
 
 
 def create_notebook(section):
+    path = 'C++'
     aux = ''
     with open('notebook.tex', 'a') as texfile:
 
         for (item, subsection) in section:
             aux += '\\section{%s}\n' % item
             for file in subsection:
-                aux += '\\includes{%s}{C++/%s/%s}\n' % (file[:-4], item, file)
+                name, ext = os.path.splitext(file)
+                if ext == '.cpp':
+                    aux += '\\includes{%s}{%s}\n' % \
+                        (name, os.path.join(path, item, file))
+                elif ext == '.tex':
+                    aux += '\\subsection{%s}' % name
+                    aux += '\\input{%s}\n' % \
+                        (os.path.join(path, item, file))
 
         aux += '\n\\end{multicols}\n\\end{document}\n'
         texfile.write(aux)
