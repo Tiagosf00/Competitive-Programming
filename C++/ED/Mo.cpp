4e730c2d289fee45956fd371fb5097e74e39a431
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2020-02-26 01:23:16 -0300

    arrumando identações

--- C++/ED/Mo.cpp
+++ C++/ED/Mo.cpp
@@ -1,39 +1,39 @@
 const int BLK = 500; // tamanho do bloco, algo entre 300 e 500 e nice
 
 struct Query {
-  int l, r, idx;
-  bool operator<(Query other) const
-  {
-    return make_pair(l / BLK, r) <
-    make_pair(other.l / BLK, other.r);
-  }
+    int l, r, idx;
+    bool operator<(Query other) const
+    {
+        return make_pair(l / BLK, r) <
+        make_pair(other.l / BLK, other.r);
+    }
 };
 
 void add(); void remove() // implementar operacoes de acordo com o problema, cuidado com TLE ao utilizar MAP
 
 vector<pair<int,ll>> mo() {
-  vector<pair<int,ll>> res;
-  sort(queries.begin(), queries.end());
+    vector<pair<int,ll>> res;
+    sort(queries.begin(), queries.end());
 
-  int l = 0, r = -1;
-  for(Query q : queries) {
-       while(l > q.l) {
-           l--;
-           add(l);
-       }
-       while(r < q.r) {
-           r++;
-           add(r);
-       }
-       while(l < q.l) {
-           remove(l);
-           l++;
-       }
-       while(r > q.r) {
-           remove(r);
-           r--;
-       }
-       res.pb(mp(q.idx, RESPOSTA)); // adicionar resposta de acordo com o problema
-   }
-   return res; // ordernar o vetor pelo indice e responder queries na ordem
+    int l = 0, r = -1;
+    for(Query q : queries) {
+        while(l > q.l) {
+                l--;
+                add(l);
+        }
+        while(r < q.r) {
+                r++;
+                add(r);
+        }
+        while(l < q.l) {
+                remove(l);
+                l++;
+        }
+        while(r > q.r) {
+                remove(r);
+                r--;
+        }
+        res.pb(mp(q.idx, RESPOSTA)); // adicionar resposta de acordo com o problema
+     }
+     return res; // ordernar o vetor pelo indice e responder queries na ordem
 }
--- C++/Math/Recursao-linear.cpp
+++ C++/Math/Recursao-linear.cpp
@@ -1,50 +1,50 @@
 vector<vl> mult(vector<vl> a, vector<vl> b, int n) {
-  vector<vl> res;
-  for(int i = 0; i < n; i++) {
-    vl tmp;
-    for(int j = 0; j < n; j++) {
-      tmp.pb(0);
+    vector<vl> res;
+    for(int i = 0; i < n; i++) {
+        vl tmp;
+        for(int j = 0; j < n; j++) {
+            tmp.pb(0);
+        }
+        res.pb(tmp);
     }
-    res.pb(tmp);
-  }
 
-  for(int row = 0; row < n; row++) {
-    for(int col = 0; col < n; col++) {
-      ll val = 0;
-      for(int k = 0; k < n; k++) {
-        val += (a[row][k]*b[k][col]);
-      }
-      res[row][col] = val;
+    for(int row = 0; row < n; row++) {
+        for(int col = 0; col < n; col++) {
+            ll val = 0;
+            for(int k = 0; k < n; k++) {
+                val += (a[row][k]*b[k][col]);
+            }
+            res[row][col] = val;
+        }
     }
-  }
 
-  return res;
+    return res;
 }
 
 vector<vl> fexp(vector<vl> b, ll e, int n) {
-  if(e == 0) {
-    vector<vl> id;
-    for(int i = 0; i < n; i++) {
-      vl tmp;
-      for(int j = 0; j < n; j++) {
-        if(i == j)
-          tmp.pb(1);
-        else
-          tmp.pb(0);
-      }
-      id.pb(tmp);
+    if(e == 0) {
+        vector<vl> id;
+        for(int i = 0; i < n; i++) {
+            vl tmp;
+            for(int j = 0; j < n; j++) {
+                if(i == j)
+                    tmp.pb(1);
+                else
+                    tmp.pb(0);
+            }
+            id.pb(tmp);
+        }
+
+        return id;
     }
 
-    return id;
-  }
+    vector<vl> res = fexp(b, e/2, n);
+    res = mult(res, res, n);
 
-  vector<vl> res = fexp(b, e/2, n);
-  res = mult(res, res, n);
+    if(e%2)
+        res = mult(res, b, n);
 
-  if(e%2)
-    res = mult(res, b, n);
-
-  return res;
+    return res;
 }
 
 // k = tamanho da recorrencia/matriz, n = n-esimo termo
@@ -52,29 +52,29 @@ vector<vl> fexp(vector<vl> b, ll e, int n) {
 // base -> [f(k-1), f(k-2), ..., f(0)]
 // coeficientes -> [c1, c2, ..., ck]
 vl solve(int k, int n, vl base, vl coef) {
-  vector<vl> inicial;
-  inicial.pb(coef);
-  for(int row = 0; row < k-1; row++) {
-    vl tmp;
-    for(int col = 0; col < k; col++) {
-      if(col == row)
-        tmp.pb(1);
-      else
-        tmp.pb(0);
+    vector<vl> inicial;
+    inicial.pb(coef);
+    for(int row = 0; row < k-1; row++) {
+        vl tmp;
+        for(int col = 0; col < k; col++) {
+            if(col == row)
+                tmp.pb(1);
+            else
+                tmp.pb(0);
+        }
+        inicial.pb(tmp);
     }
-    inicial.pb(tmp);
-  }
 
-  vector<vl> matexp = fexp(inicial, max(0, n-k+1), k);
-  vl res(k);
+    vector<vl> matexp = fexp(inicial, max(0, n-k+1), k);
+    vl res(k);
 
-  for(int row = 0; row < k; row++) {
-    ll val = 0;
-    for(int aux = 0; aux < k; aux++) {
-      val += matexp[row][aux]*base[aux];
+    for(int row = 0; row < k; row++) {
+        ll val = 0;
+        for(int aux = 0; aux < k; aux++) {
+            val += matexp[row][aux]*base[aux];
+        }
+        res[row] = val; // res = (f(n), f(n-1), ..., f(n-k+1))
     }
-    res[row] = val; // res = (f(n), f(n-1), ..., f(n-k+1))
-  }
 
-  return res;
+    return res;
 }
