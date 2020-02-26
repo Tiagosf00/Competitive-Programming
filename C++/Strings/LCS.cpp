d46783098786f70518366805483fbda3917db160
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2020-02-26 01:01:02 -0300

    LCS string

---
+++ C++/Strings/LCS.cpp
@@ -0,0 +1,42 @@
+string LCSubStr(string X, string Y)
+{
+    int m = X.size();
+    int n = Y.size();
+  
+    int result = 0, end;
+    int len[2][n];
+    int currRow = 0;
+
+    for(int i=0;i<=m;i++){
+        for(int j=0;j<=n;j++){
+            if(i==0 || j==0)
+                len[currRow][j] = 0;
+            else if(X[i-1] == Y[j-1]){
+                len[currRow][j] = len[1-currRow][j-1] + 1;
+                if(len[currRow][j] > result){
+                    result = len[currRow][j];
+                    end = i - 1;
+                }
+            }
+            else
+                len[currRow][j] = 0;
+        }
+
+        currRow = 1 - currRow;
+    }
+
+    if(result==0)
+        return string();
+  
+    return X.substr(end - result + 1, result);
+}
+
+int main() 
+{ 
+    string X = "GeeksforGeeks"; 
+    string Y = "GeeksQuiz"; 
+
+    cout << LCSubStr(X, Y) << endl;
+
+    return 0; 
+} 
