664cf86d8b4da729f886f6a7f587305cdb6c8c20
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2020-02-26 12:43:41 -0300

    adicionando template

---
+++ C++/Misc/Template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define ff first
+#define ss second
+#define ll long long
+#define ld long double
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define mt make_tuple
+#define pii pair<int, int>
+#define vi vector<int>
+#define sws ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define endl '\n'
+#define teto(a, b) (a+b-1)/(b)
+
+const int MAX = 400010;
+const int MOD = 1e9+7;
+const int INF = 0x3f3f3f3f;
+const ll LLINF = 0x3f3f3f3f3f3f3f3f;
+const ld EPS = 1e-7;
+
+using namespace std;
