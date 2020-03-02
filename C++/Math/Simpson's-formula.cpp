68778c8036c573ea59c68a337c959436425b5341
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2020-03-02 17:52:08 -0300

    simpsons do deus jose

--- C++/Math/Simpson's-formula.cpp
+++ C++/Math/Simpson's-formula.cpp
@@ -1,13 +1,21 @@
-const int N = 1e6;
+inline ld simpson(ld fl, ld fr, ld fmid, ld l, ld r){
+	return (fl+fr+4*fmid)*(r-l)/6;
+}
 
-ld simpson_integration(ld a, ld b)
+ld rsimpson(ld slr, ld fl, ld fr, ld fmid, ld l, ld r)
 {
-    ld h = (b - a) / N;
-    ld s = f(a) + f(b); // a = x_0 and b = x_2n
-    for(int i=1;i<N;i++){ // Final Simpson's formula
-        ld x = a + h*i;
-        s += f(x)*((i & 1) ? 4 : 2);
-    }
-    s*=h/3;
-    return s;
+	ld mid = (l+r)/2;
+	ld fml = f((l+mid)/2), fmr = f((mid+r)/2);
+	ld slm = simpson(fl,fmid,fml,l,mid);
+	ld smr = simpson(fmid,fr,fmr,mid,r);
+	if(fabsl(slr-slm-smr) < EPS) return slm+smr; // aprox. good enough
+	return rsimpson(slm,fl,fmid,fml,l,mid)+rsimpson(smr,fmid,fr,fmr,mid,r);
+}
+
+ld integrate(ld l, ld r)
+{
+	ld mid = (l+r)/2;
+	ld fl = f(l), fr = f(r);
+	ld fmid = f(mid);
+	return rsimpson(simpson(fl,fr,fmid,l,r),fl,fr,fmid,l,r);
 }
