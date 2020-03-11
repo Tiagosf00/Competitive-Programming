91adf208cc9df054a1aa68cf37835e732d4ca978
Alberto Duarte <albertotdneto@hotmail.com>
2020-03-11 17:38:42 -0300

    Update Pal-int.cpp

--- C++/Strings/Pal-int.cpp
+++ C++/Strings/Pal-int.cpp
@@ -1,5 +1,4 @@
-bool ehpalindromo(ll n) 
-{
+bool ehpalindromo(ll n) {
     if(n<0)
         return false;
 
@@ -7,8 +6,7 @@ bool ehpalindromo(ll n)
     while(n/divisor >= 10) 
         divisor *= 10; 
   
-    while(n != 0) 
-    { 
+    while(n != 0) { 
         int leading = n / divisor;
         int trailing = n % 10;
 
