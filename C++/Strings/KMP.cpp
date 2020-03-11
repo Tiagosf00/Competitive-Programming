4f19a502f228a80f89673431d0dd73533ccc571c
Alberto Duarte <albertotdneto@hotmail.com>
2020-03-11 17:37:27 -0300

    Update KMP.cpp

--- C++/Strings/KMP.cpp
+++ C++/Strings/KMP.cpp
@@ -1,22 +1,22 @@
 vector<int> preffix_function(const string &s){
-	int n = s.size(); vector<int> b(n+1);
-	b[0] = -1; int i = 0, j = -1;
-	while(i < n){
-		while(j >= 0 && s[i] != s[j]) j = b[j];
-		b[++i] = ++j;
+    int n = s.size(); vector<int> b(n+1);
+    b[0] = -1; int i = 0, j = -1;
+    while(i < n){
+        while(j >= 0 && s[i] != s[j]) j = b[j];
+            b[++i] = ++j;
 	}
-	return b;
+    return b;
 }
-void kmp(const string &t, const string &p){
-	vector<int> b = preffix_function(p);
-	int n = t.size(), m = p.size();
-	int j = 0;
-	for(int i = 0; i < n; i++){
-		while(j >= 0 && t[i] != p[j]) j = b[j];
-		j++;
-		if(j == m){
 
-			j = b[j];
-		}
+void kmp(const string &t, const string &p){
+    vector<int> b = preffix_function(p);
+    int n = t.size(), m = p.size();
+    int j = 0;
+    for(int i = 0; i < n; i++){
+        while(j >= 0 && t[i] != p[j]) j = b[j];
+	j++;
+	if(j == m) {
+	    j = b[j];
 	}
+    }
 }
