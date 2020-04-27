af870ef86e20f6f0a81fbb9646797897c4f22cf2
andersonvieiradonascimento <andersonkuro@gmail.com>
2020-04-27 04:49:26 -0300

    Adding Unbounded Knapsack to Algoritmos

---
+++ C++/Algoritmos/Unbounded-Knapsack.cpp
@@ -0,0 +1,17 @@
+int w, n;
+int c[MAX], v[MAX];
+
+int unbounded_knapsack()
+{
+
+    int dp[w+1];
+    memset(dp, 0, sizeof dp);
+
+    for(int i=0;i<=w;i++)
+        for(int j=0;j<n;j++)
+            if(c[j] <= i)
+                dp[i] = max(dp[i], dp[i-c[j]] + v[j]);
+
+    return dp[w];
+
+}
