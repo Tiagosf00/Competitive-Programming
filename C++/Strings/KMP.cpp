9b958e3b5c88006db5bf93e5c4ffea58775e83ad
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2020-05-27 21:25:54 -0300

    kmp novinho

--- C++/Strings/KMP.cpp
+++ C++/Strings/KMP.cpp
@@ -1,22 +1,21 @@
-vector<int> preffix_function(const string &s){
-    int n = s.size(); vector<int> b(n+1);
-    b[0] = -1; int i = 0, j = -1;
-    while(i < n){
-        while(j >= 0 && s[i] != s[j]) j = b[j];
-            b[++i] = ++j;
-	}
-    return b;
+vi pi(const string &s){
+    int n=s.size();
+    vi p(n);
+    for(int i=1, j=0; i<n; i++){
+        while(j>0 and s[i]!=s[j]) j=p[j-1];
+        if(s[j]==s[i]) j++;
+        p[i]=j;
+    }
+    return p;
 }
 
-void kmp(const string &t, const string &p){
-    vector<int> b = preffix_function(p);
-    int n = t.size(), m = p.size();
-    int j = 0;
-    for(int i = 0; i < n; i++){
-        while(j >= 0 && t[i] != p[j]) j = b[j];
-	j++;
-	if(j == m) {
-	    j = b[j];
-	}
+vi kmp(const string &t, const string &s){
+    vi p = pi(s+'$'), match;
+    int n=t.size(), m=s.size();
+    for(int i=0, j=0; i<n; i++){
+        while(j>0 and t[i]!=s[j]) j=p[j-1];
+        if(t[i]==s[j]) j++;
+        if(j==m) match.pb(i-j+1);
     }
+    return match;
 }
