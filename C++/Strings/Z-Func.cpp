954bc8e0806eb4f3d02b08451cd8c1646a639329
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2020-05-27 21:36:30 -0300

    comentario na zfunc

--- C++/Strings/Z-Func.cpp
+++ C++/Strings/Z-Func.cpp
@@ -1,16 +1,14 @@
-vector<int> z_algo(const string &s)
-{
+vi z_algo(const string &s)
+{   // returns vector for each idx where a prefix of size i starts.
 	int n = s.size();
 	int L = 0, R = 0;
-	vector<int> z(n, 0);
-	for(int i = 1; i < n; i++)
-	{
+	vi z(n, 0);
+	for(int i = 1; i < n; i++){
 		if(i <= R)
 			z[i] = min(z[i-L], R - i + 1);
-		while(z[i]+i < n && s[ z[i]+i ] == s[ z[i] ])
+		while(z[i]+i < n and s[ z[i]+i ] == s[ z[i] ])
 			z[i]++;
-		if(i+z[i]-1 > R)
-		{
+		if(i+z[i]-1 > R){
 			L = i;
 			R = i + z[i] - 1;
 		}
