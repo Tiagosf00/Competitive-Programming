6a0fb6c9802f72f5f0bcb7dea10839647c0e1bcd
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2020-07-02 16:35:46 -0300

    arrumando identação

--- C++/Math/Totient.cpp
+++ C++/Math/Totient.cpp
@@ -1,41 +1,38 @@
 // phi(p^k) = (p^(k-1))*(p-1) com p primo
 // O(sqrt(m))
-ll phi(ll m) {
+ll phi(ll m){
     ll res = m;
-    for(ll d = 2; d*d <= m; d++) {
-      if(m % d == 0) {
-          res = (res/d) * (d-1);
-          while(m % d == 0) {
-            m /= d;
-          }
-      }
+    for(ll d=2;d*d<=m;d++){
+        if(m % d == 0){
+            res = (res/d)*(d-1);
+            while(m%d == 0)
+                m /= d;
+        }
     }
     if(m > 1) {
-      res /= m;
-      res *= (m-1);
+        res /= m;
+        res *= (m-1);
     }
-
     return res;
 }
 
 // modificacao do crivo, O(n*log(log(n)))
-vector<ll> phi_to_n(ll n) {
+vl phi_to_n(ll n){
     vector<bool> isprime(n+1, true);
-    vector<ll> tot(n+1);
+    vl tot(n+1);
     tot[0] = 0; tot[1] = 1;
-    for(ll i = 1; i <= n; i++) {
-      tot[i] = i;
+    for(ll i=1;i<=n; i++){
+        tot[i] = i;
     }
 
-for(ll p = 2; p <= n; p++) {
-    if(isprime[p]) {
-      tot[p] = p-1;
-      for(ll i = p+p; i <= n; i += p) {
-          isprime[i] = false;
-          tot[i] = (tot[i]/p)*(p-1);
-      }
+    for(ll p=2;p<=n;p++){
+        if(isprime[p]){
+            tot[p] = p-1;
+            for(ll i=p+p;i<=n;i+=p){
+                isprime[i] = false;
+                tot[i] = (tot[i]/p)*(p-1);
+            }
+        }
     }
-}
-
     return tot;
 }
