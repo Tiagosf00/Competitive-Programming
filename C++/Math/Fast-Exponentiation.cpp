f99d8b2c978c248092d2467daacc43d81228449d
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2020-07-02 15:44:17 -0300

    arrumando identação

--- C++/Math/Fast-Exponentiation.cpp
+++ C++/Math/Fast-Exponentiation.cpp
@@ -1,14 +1,10 @@
 // Modular exponentiaion - (x^y)%mod in O(log y)
-ll power(ll x, ll y, ll mod) 
-{
+ll power(ll x, ll y, ll mod){
     ll res = 1;
     x%=mod;
-
-    while(y)
-    { 
+    while(y){ 
         if(y&1)
             res=(res*x)%mod;
-  
         y=y>>1;
         x=(x*x)%mod;
     }
