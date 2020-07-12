e2ed6bb65008af812a542e86a9036c664a700b1d
Alberto Duarte <albertotdneto@hotmail.com>
2020-07-11 22:07:32 -0300

    multiplicação modular log

---
+++ C++/Math/Mulmod.cpp
@@ -0,0 +1,14 @@
+ll mulmod(ll a, ll b) {
+    if(a == 0) {
+        return 0LL;
+    }
+    if(a%2 == 0) {
+        ll val = mullog(a/2, b);
+        return (val + val) % MOD;
+    }
+    else {
+        ll val = mullog((a-1)/2, b);
+        val = (val + val) % MOD;
+        return (val + b) % MOD;
+    }
+}
