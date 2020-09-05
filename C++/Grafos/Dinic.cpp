608f6281e1eec404a7859417b60d8a91c5e23089
Alberto Duarte <albertotdneto@hotmail.com>
2020-09-04 22:10:31 -0300

    Create Dinic.cpp

---
+++ C++/Grafos/Dinic.cpp
@@ -0,0 +1,77 @@
+const int N = 300;
+
+struct Dinic {
+    struct Edge{
+        int from, to; ll flow, cap;
+    };
+    vector<Edge> edge;
+
+    vector<int> g[N];
+    int ne = 0;
+    int lvl[N], vis[N], pass;
+    int qu[N], px[N], qt;
+
+    ll run(int s, int sink, ll minE) {
+        if(s == sink) return minE;
+
+        ll ans = 0;
+
+        for(; px[s] < (int)g[s].size(); px[s]++) {
+            int e = g[s][ px[s] ];
+            auto &v = edge[e], &rev = edge[e^1];
+            if(lvl[v.to] != lvl[s]+1 || v.flow >= v.cap)
+                continue;           // v.cap - v.flow < lim
+            ll tmp = run(v.to, sink,min(minE, v.cap-v.flow));
+            v.flow += tmp, rev.flow -= tmp;
+            ans += tmp, minE -= tmp;
+            if(minE == 0) break;
+        }
+        return ans;
+    }
+    bool bfs(int source, int sink) {
+        qt = 0;
+        qu[qt++] = source;
+        lvl[source] = 1;
+        vis[source] = ++pass;
+        for(int i = 0; i < qt; i++) {
+            int u = qu[i];
+            px[u] = 0;
+    		if(u == sink) return true;
+            for(auto& ed : g[u]) {
+                auto v = edge[ed];
+                if(v.flow >= v.cap || vis[v.to] == pass)
+                    continue; // v.cap - v.flow < lim
+                vis[v.to] = pass;
+                lvl[v.to] = lvl[u]+1;
+                qu[qt++] = v.to;
+            }
+        }
+        return false;
+    }
+    ll flow(int source, int sink) {
+        reset_flow();
+        ll ans = 0;
+        //for(lim = (1LL << 62); lim >= 1; lim /= 2)
+        while(bfs(source, sink))
+    		ans += run(source, sink, LLINF);
+        return ans;
+    }
+    void addEdge(int u, int v, ll c, ll rc) {
+        Edge e = {u, v, 0, c};
+        edge.pb(e);
+        g[u].push_back(ne++);
+
+        e = {v, u, 0, rc};
+        edge.pb(e);
+        g[v].push_back(ne++);
+    }
+    void reset_flow() {
+    	for(int i = 0; i < ne; i++)
+    		edge[i].flow = 0;
+        memset(lvl, 0, sizeof(lvl));
+        memset(vis, 0, sizeof(vis));
+        memset(qu, 0, sizeof(qu));
+        memset(px, 0, sizeof(px));
+        qt = 0; pass = 0;
+    }
+};
