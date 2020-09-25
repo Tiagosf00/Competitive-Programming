52d728c00f8ec7605a916126fcc415f1f64fb359
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2020-09-25 00:29:51 -0300

    kruskal with our dsu

--- C++/Grafos/Kruskal.cpp
+++ C++/Grafos/Kruskal.cpp
@@ -1,4 +1,4 @@
-// deve-se ter dsu codada com as funcoes make_set, find_set e union_sets
+// Uses DSU .join() and .find()
 struct Edge {
     int u, v, weight;
     bool operator<(Edge const& other) {
@@ -7,19 +7,16 @@ struct Edge {
 };
 
 int n;
-vector<Edge> edges;
-
+DSU dsu(n);
+vector<Edge> edges, result;
 int cost = 0;
-vector<Edge> result;
-for (int i = 0; i < n; i++)
-    make_set(i);
 
 sort(edges.begin(), edges.end());
 
-for (Edge e : edges) {
-    if (find_set(e.u) != find_set(e.v)) {
+for(auto e : edges) {
+    if (dsu.find(e.u) != dsu.find(e.v)) {
         cost += e.weight;
         result.push_back(e); // vector com as arestas da MST
-        union_sets(e.u, e.v);
+        dsu.join(e.u, e.v);
     }
 }
