92380adca4b5328bcd5dce276c0477f6045110c1
Alberto Duarte <albertotdneto@hotmail.com>
2021-03-08 23:12:16 -0300

    Atualizando exponenciação de matriz com struct top

---
+++ C++/Math/Exponenciacao-matriz.cpp
@@ -0,0 +1,44 @@
+struct Matrix {
+    vector<vl> m;
+    int r, c;
+
+    Matrix(vector<vl> mat) {
+        m = mat;
+        r = mat.size();
+        c = mat[0].size();
+    }
+
+    Matrix(int row, int col, bool ident=false) {
+        r = row; c = col;
+        m = vector<vl>(r, vi(c, 0));
+        if(ident) {
+            for(int i = 0; i < min(r, c); i++) {
+                m[i][i] = 1;
+            }
+        }
+    }
+
+    Matrix operator*(const Matrix &o) const {
+        assert(c == o.r); // garantir que da pra multiplicar
+        vector<vl> res(r, vi(o.c, 0));
+
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < o.c; j++) {
+                for(int k = 0; k < c; k++) {
+                    res[i][j] = (res[i][j] + m[i][k]*o.m[k][j]) % MOD;
+                }
+            }
+        }
+
+        return Matrix(res);
+    }
+};
+
+Matrix fexp(Matrix b, int e, int n) {
+    if(e == 0) return Matrix(n, n, true); // identidade
+    Matrix res = fexp(b, e/2, n);
+    res = (res * res);
+    if(e%2) res = (res * b);
+
+    return res;
+}
