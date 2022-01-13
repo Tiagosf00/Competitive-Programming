f39026ceb8e495d90e7507ef5ea7328888c73895
Alberto Duarte <albertotdneto@hotmail.com>
2022-01-13 17:08:34 -0300

    Add gauss xor

---
+++ C++/Math/GaussXor.cpp
@@ -0,0 +1,22 @@
+struct Gauss {
+    array<ll, LOG_MAX> vet;
+    int size;
+    Gauss() : size(0) {
+		fill(vet.begin(), vet.end(), 0);
+	}
+    Gauss(vector<ll> vals) : size(0) {
+		fill(vet.begin(), vet.end(), 0);
+        for(ll val : vals) add(val);
+    }
+    bool add(ll val) {
+        for(int i = LOG_MAX-1; i >= 0; i--) if(val & (1LL << i)) {
+            if(vet[i] == 0) {
+                vet[i] = val;
+                size++;
+                return true;
+            }
+            val ^= vet[i];
+        }
+        return false;
+    }
+};
