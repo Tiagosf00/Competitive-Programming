024c4a4276cbf41ba2894744250d7e6b6a83b42c
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2022-02-01 23:44:53 -0300

    simpson and mcmf

--- C++/Numeric/Simpson's-formula.cpp
+++ C++/Numeric/Simpson's-formula.cpp
@@ -1,21 +1,21 @@
 inline ld simpson(ld fl, ld fr, ld fmid, ld l, ld r){
-	return (fl+fr+4*fmid)*(r-l)/6;
+    return (fl+fr+4*fmid)*(r-l)/6;
 }
 
 ld rsimpson(ld slr, ld fl, ld fr, ld fmid, ld l, ld r)
 {
-	ld mid = (l+r)/2;
-	ld fml = f((l+mid)/2), fmr = f((mid+r)/2);
-	ld slm = simpson(fl,fmid,fml,l,mid);
-	ld smr = simpson(fmid,fr,fmr,mid,r);
-	if(fabsl(slr-slm-smr) < EPS) return slm+smr; // aprox. good enough
-	return rsimpson(slm,fl,fmid,fml,l,mid)+rsimpson(smr,fmid,fr,fmr,mid,r);
+    ld mid = (l+r)/2;
+    ld fml = f((l+mid)/2), fmr = f((mid+r)/2);
+    ld slm = simpson(fl,fmid,fml,l,mid);
+    ld smr = simpson(fmid,fr,fmr,mid,r);
+    if(fabsl(slr-slm-smr) < EPS) return slm+smr; // aprox. good enough
+    return rsimpson(slm,fl,fmid,fml,l,mid)+rsimpson(smr,fmid,fr,fmr,mid,r);
 }
 
 ld integrate(ld l, ld r)
 {
-	ld mid = (l+r)/2;
-	ld fl = f(l), fr = f(r);
-	ld fmid = f(mid);
-	return rsimpson(simpson(fl,fr,fmid,l,r),fl,fr,fmid,l,r);
+    ld mid = (l+r)/2;
+    ld fl = f(l), fr = f(r);
+    ld fmid = f(mid);
+    return rsimpson(simpson(fl,fr,fmid,l,r),fl,fr,fmid,l,r);
 }
