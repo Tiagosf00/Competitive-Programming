c36ce474c769f4cd4884c64a498dc83198fa4e18
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2022-02-19 18:23:03 -0300

    crivo tava errado :(

--- C++/Math/Crivo.cpp
+++ C++/Math/Crivo.cpp
@@ -1,8 +1,6 @@
-// Sieve of Eratosthenes
-
-vi p(MAX, true);
-
-for(ll i=4; i<MAX; i++) p[i] = 2;
+vi p(MAX, 0);
+p[0] = p[1] = 1;
+for(ll i=4; i<MAX; i+=2) p[i] = 2;
 for(ll i=3; i<MAX; i+=2)
     if(!p[i])
         for(ll j=i*i; j<MAX; j+=2*i)
