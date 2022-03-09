08ab79122d6573c3bbfba6a0cdc01edceaccd168
Leonardo Riether <leonardoalves8217@gmail.com>
2022-03-08 22:34:22 -0300

    kmp mais novinho ainda

--- C++/Strings/KMP.cpp
+++ C++/Strings/KMP.cpp
@@ -1,21 +1,12 @@
-vi pi(const string &s){
-    int n=s.size();
-    vi p(n);
-    for(int i=1, j=0; i<n; i++){
-        while(j>0 and s[i]!=s[j]) j=p[j-1];
-        if(s[j]==s[i]) j++;
-        p[i]=j;
-    }
-    return p;
+string p;
+int neighbor[N];
+int walk(int u, char c) { // leader after inputting `c`
+    while (u != -1 && (u+1 >= (int)p.size() || p[u + 1] != c)) // leader doesn't match
+        u = neighbor[u];
+    return p[u + 1] == c ? u+1 : u;
 }
-
-vi kmp(const string &t, const string &s){
-    vi p = pi(s+'$'), match;
-    int n=t.size(), m=s.size();
-    for(int i=0, j=0; i<n; i++){
-        while(j>0 and t[i]!=s[j]) j=p[j-1];
-        if(t[i]==s[j]) j++;
-        if(j==m) match.pb(i-j+1);
-    }
-    return match;
+void build() {
+    neighbor[0] = -1; // -1 is the leftmost state
+    for (int i = 1; i < (int)p.size(); i++)
+        neighbor[i] = walk(neighbor[i-1], p[i]);
 }
