005f3d7ea0b8c5c2666550aea652c83bcbc62982
LeoRiether <leonardoalves8217@gmail.com>
2022-03-14 18:09:40 -0300

    Lowercase names

--- rename.py
+++ rename.py
@@ -12,15 +12,14 @@ def files(root):
                 yield x
 
 for file in files("C++"):
-    # if not file[-5:] == ".what":
-    #     continue
+    if not file[-5:] == ".what":
+        continue
     after = file.replace("-", "_")
     s = after.split('/')
     if s[-1][:9] != "README.md":
         s[-1] = s[-1].lower()
     after = os.path.join(*s)
-    after += ".what"
-    # after = after[:-5]
+    after = after[:-5]
     if file != after:
         print(f"{file} -> {after}")
         os.replace(file, after)
