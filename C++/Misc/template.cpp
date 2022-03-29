700842f4f3d353e4fcafeba00afd10463f16d57f
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2022-03-29 19:11:43 -0300

    removing dsu from notebook and modifying template

--- C++/Misc/template.cpp
+++ C++/Misc/template.cpp
@@ -1,30 +1,11 @@
 #include <bits/stdc++.h>
-#define ff first
-#define ss second
-#define ll long long
-#define ld long double
-#define pb push_back
-#define eb emplace_back
-#define mp make_pair
-#define mt make_tuple
-#define pii pair<int, int>
-#define vi vector<int>
-#define vl vector<ll>
-#define vii vector<pii>
-#define sws ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-#define endl '\n'
-#define teto(a, b) ((a+b-1)/(b))
-#define all(x) x.begin(), x.end()
-#define forn(i, n) for(int i = 0; i < (int)n; i++)
-#define forne(i, a, b) for(int i = a; i <= b; i++)
-#define dbg(msg, var) cerr << msg << " " << var << endl;
-
 using namespace std;
 
-const int MAX = 200010;
-const int MOD = 1000000007;
-const int INF = 1e8;
-const ll LLINF = 0x3f3f3f3f3f3f3f3f;
-const ld EPS = 1e-7;
+using ll = long long;
+using ii = pair<int, int>;
+using vi = vector<int>;
+#define pb emplace_back
+#define mp make_pair
 
-// End Template //
+const ll MOD = 998'244'353;
+const int MAX = 2e5 + 5;
