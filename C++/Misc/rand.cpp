3410207389cac4c2b89968d92a386331f07d4eaa
Leonardo Riether <leonardoalves8217@gmail.com>
2022-05-10 11:23:16 -0300

    Comment about mt19937_64

--- C++/Misc/rand.cpp
+++ C++/Misc/rand.cpp
@@ -1,4 +1,4 @@
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count()); // mt19937_64
 uniform_int_distribution<int> distribution(1,n);
 
 num = distribution(rng); // num no range [1, n]
