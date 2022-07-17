e245e0f963ecedf7713cf875dad6cafc4f0e21f5
Leonardo Riether <leonardoalves8217@gmail.com>
2022-07-17 12:07:43 -0300

    convex_hull: U[U.size()-2] => U.end()[-2]

--- C++/Geometria/convex_hull.cpp
+++ C++/Geometria/convex_hull.cpp
@@ -3,13 +3,13 @@ vp convex_hull(const vp P)
     sort(P.begin(), P.end());
     vp L, U;
     for(auto p: P){
-        while(L.size()>=2 and ccw(L[L.size()-2], L.back(), p)!=1)
+        while(L.size()>=2 and ccw(L.end()[-2], L.back(), p)!=1)
             L.pop_back();
         L.pb(p);
     }
     reverse(P.begin(), P.end());
     for(auto p: P){
-        while(U.size()>=2 and ccw(U[U.size()-2], U.back(), p)!=1)
+        while(U.size()>=2 and ccw(U.end()[-2], U.back(), p)!=1)
             U.pop_back();
         U.pb(p);
     }
