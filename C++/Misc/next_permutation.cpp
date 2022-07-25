923bbd72894a3022130ac72afaaaaa002605e528
Victor Manuel <victorm@lacunasoftware.com>
2022-07-25 18:14:29 -0300

    Fix example output order in comment

--- C++/Misc/next_permutation.cpp
+++ C++/Misc/next_permutation.cpp
@@ -1,5 +1,5 @@
 vector<int> a = {1, 2, 3};
 int n = a.size();
 do{ 
-    display(a, n);// 1,2,3; 1,3,2; 2,1,3; 3,1,2; 2,3,1; 3,2,1; 
+    display(a, n);// 1,2,3; 1,3,2; 2,1,3; 2,3,1; 3,1,2; 3,2,1; 
 }while(next_permutation(a.begin(), a.begin() + n)); 
