b3681d018c2a597d505b25abb9948fb52b99a842
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2022-08-25 22:10:35 -0300

    suffixarray

--- C++/Strings/suffix_array.cpp
+++ C++/Strings/suffix_array.cpp
@@ -1,29 +1,34 @@
-vi suffix_array(string s){
-    s.pb('$');
-    int n = s.size();
-
-    vi p(n), c(n);
-    vector< pair<char, int> > a(n);
-    for(int i=0;i<n;i++) a[i] = {s[i], i};
-    sort(a.begin(), a.end());
-    
-    for(int i=0;i<n;i++) p[i] = a[i].ss;
-    c[p[0]]=0;
-    for(int i=1;i<n;i++)
-        c[p[i]] = c[p[i-1]] + (a[i].ff!=a[i-1].ff);
-
-    int k=0;
-    while((1<<k) < n){
-        vector< pair<pii, int> > a(n);
-        for(int i=0;i<n;i++)
-            a[i] = {{c[i], c[(i+(1<<k))%n]}, i};
-        sort(a.begin(), a.end());
-
-        for(int i=0;i<n;i++) p[i] = a[i].ss;
-        c[p[0]]=0;
-        for(int i=1;i<n;i++)
-            c[p[i]] = c[p[i-1]] + (a[i].ff!=a[i-1].ff);
-        k++;
-    }
-    return p;
+vector<int> suffix_array(string s) {
+	s += "$";
+	int n = s.size(), N = max(n, 260);
+	vector<int> sa(n), ra(n);
+	for (int i = 0; i < n; i++) sa[i] = i, ra[i] = s[i];
+ 
+	for (int k = 0; k < n; k ? k *= 2 : k++) {
+		vector<int> nsa(sa), nra(n), cnt(N);
+ 
+		for (int i = 0; i < n; i++) nsa[i] = (nsa[i]-k+n)%n, cnt[ra[i]]++;
+		for (int i = 1; i < N; i++) cnt[i] += cnt[i-1];
+		for (int i = n-1; i+1; i--) sa[--cnt[ra[nsa[i]]]] = nsa[i];
+ 
+		for (int i = 1, r = 0; i < n; i++) nra[sa[i]] = r += ra[sa[i]] !=
+			ra[sa[i-1]] or ra[(sa[i]+k)%n] != ra[(sa[i-1]+k)%n];
+		ra = nra;
+		if (ra[sa[n-1]] == n-1) break;
+	}
+	return vector<int>(sa.begin()+1, sa.end());
+}
+ 
+vector<int> kasai(string s, vector<int> sa) {
+	int n = s.size(), k = 0;
+	vector<int> ra(n), lcp(n);
+	for (int i = 0; i < n; i++) ra[sa[i]] = i;
+ 
+	for (int i = 0; i < n; i++, k -= !!k) {
+		if (ra[i] == n-1) { k = 0; continue; }
+		int j = sa[ra[i]+1];
+		while (i+k < n and j+k < n and s[i+k] == s[j+k]) k++;
+		lcp[ra[i]] = k;
+	}
+	return lcp;
 }
