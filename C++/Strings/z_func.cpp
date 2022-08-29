a4b20fcfb8f682ae662b24d4bf7adcc54d41be3b
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2022-08-29 02:34:56 -0300

    adjusting identation

--- C++/Strings/z_func.cpp
+++ C++/Strings/z_func.cpp
@@ -1,12 +1,12 @@
-vi Z(string s) {
-  int n = s.size();
-  vi z(n);
-  int x = 0, y = 0;
-  for (int i = 1; i < n; i++) {
-    z[i] = max(0, min(z[i - x], y - i + 1));
-    while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
-      x = i; y = i + z[i]; z[i]++;
+vector<int> Z(string s) {
+    int n = s.size();
+    vector<int> z(n);
+    int x = 0, y = 0;
+    for (int i = 1; i < n; i++) {
+        z[i] = max(0, min(z[i - x], y - i + 1));
+        while (i + z[i] < n and s[z[i]] == s[i + z[i]]) {
+            x = i; y = i + z[i]; z[i]++;
+        }
     }
-  }
-  return z;
+    return z;
 }
