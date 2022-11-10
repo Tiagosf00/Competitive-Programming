1d22d1ea86e6fd87c414f1639539a35dd3016cf3
Leonardo Riether <leonardoalves8217@gmail.com>
2022-11-10 17:18:51 -0300

    New & shiny 2SAT
    featuring atMostOne() and ~x!

--- C++/Grafos/2sat.cpp
+++ C++/Grafos/2sat.cpp
@@ -1,45 +1,50 @@
-vector<int> g[MAX], gt[MAX], S; int vis[MAX], cor[MAX];
-
-int val(int n, bool tvalue) {
-    if(tvalue) return 2*n;
-    return 2*n +1;
-}
-
-void dfs(int u) {
-    vis[u] = 1; for(int v : g[u]) if(!vis[v]) dfs(v);
-    S.push_back(u);
-}
-
-void dfst(int u, int e) {
-    cor[u] = e;
-    for(int v : gt[u]) if(!cor[v]) dfst(v, e);
-}
-
-void kosaraju(int n) {
-    for(int i = 0; i <= n; i++) if(!vis[i]) dfs(i);
-    for(int i = 0; i <= n; i++) for(int j : g[i])
-        gt[j].push_back(i);
-    int e = 0; reverse(S.begin(), S.end());
-    for(int u : S) if(!cor[u]) dfst(u, ++e);
-}
-
-// antes de chamar essa funcao, colocar as arestas do grafo
-bool solve(int n, vi &res) {
-    kosaraju(2*n); // MAX > 2*N
-    vi r;
-
-    forn(i, n) {
-        int t = val(i, true), f = val(i, false);
-        if(cor[t] == cor[f]) {
-            return false;
-        }
-        else {
-            if(cor[t] > cor[f])
-                r.pb(1);
-            else
-                r.pb(0);
-        }
-    }
-    swap(r, res);
-    return true;
-}
+#define rep(i,l,r) for (int i = (l); i < (r); i++)
+struct TwoSat { // copied from kth-competitive-programming/kactl
+	int N;
+	vector<vi> gr;
+	vi values; // 0 = false, 1 = true
+	TwoSat(int n = 0) : N(n), gr(2*n) {}
+	int addVar() { // (optional)
+		gr.emplace_back();
+		gr.emplace_back();
+		return N++;
+	}
+	void either(int f, int j) {
+		f = max(2*f, -1-2*f);
+		j = max(2*j, -1-2*j);
+		gr[f].push_back(j^1);
+		gr[j].push_back(f^1);
+	}
+	void atMostOne(const vi& li) { // (optional)
+		if ((int)li.size() <= 1) return;
+		int cur = ~li[0];
+		rep(i,2,(int)li.size()) {
+			int next = addVar();
+			either(cur, ~li[i]);
+			either(cur, next);
+			either(~li[i], next);
+			cur = ~next;
+		}
+		either(cur, ~li[1]);
+	}
+	vi _val, comp, z; int time = 0;
+	int dfs(int i) {
+		int low = _val[i] = ++time, x; z.push_back(i);
+		for(int e : gr[i]) if (!comp[e])
+			low = min(low, _val[e] ?: dfs(e));
+		if (low == _val[i]) do {
+			x = z.back(); z.pop_back();
+			comp[x] = low;
+			if (values[x>>1] == -1)
+				values[x>>1] = x&1;
+		} while (x != i);
+		return _val[i] = low;
+	}
+	bool solve() {
+		values.assign(N, -1);
+		_val.assign(2*N, 0); comp = _val;
+		rep(i,0,2*N) if (!comp[i]) dfs(i);
+		rep(i,0,N) if (comp[2*i] == comp[2*i+1]) return 0;
+		return 1;
+	}
+};
