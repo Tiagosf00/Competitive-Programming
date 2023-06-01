bee63629c153a109f50daaa21f8cb12907b3f6a4
Tiago de Souza Fernandes <43066400+Tiagosf00@users.noreply.github.com>
2023-06-01 11:43:24 -0300

    Update submask.cpp

--- C++/Misc/submask.cpp
+++ C++/Misc/submask.cpp
@@ -1,5 +1,16 @@
-for (int m=0; m<(1<<n); m++) {
-    for (int s=m; s; s=(s-1) & m) {
+// O(3^n)
+for (int m = 0; m < (1<<n); m++) {
+    for (int s = m; s; s = (s-1) & m) {
         // s is every submask of m
+    }
+}
+
+// O(2^n * n) SOS dp like
+for (int b = n-1; b >= 0; b--) {
+    for (int m = 0; m < (1 << n); m++) {
+        if (j & (1 << b)) {
+            // propagate info through submasks
+            amount[j ^ (1 << b)] += amount[j];
+        }
     }
 }
