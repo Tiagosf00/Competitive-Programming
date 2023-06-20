c020892aacd6d61653010742c318c49a61e662be
Tiago de Souza Fernandes <43066400+Tiagosf00@users.noreply.github.com>
2023-06-20 02:00:02 -0300

    Update bitwise.cpp

--- C++/Misc/bitwise.cpp
+++ C++/Misc/bitwise.cpp
@@ -1,13 +1,13 @@
 // Least significant bit (lsb)
-	int lsb(int x){ return x&-x; }
+    int lsb(int x){ return x&-x; }
 
 // Power of two
-	bool isPowerOfTwo(int x){ return x && (!(x&(x-1))); }
+    bool isPowerOfTwo(int x){ return x && (!(x&(x-1))); }
 
 // Built-in functions
 
-	// Number of bits 1
-	__builtin_popcount()
+    // Number of bits 1
+    __builtin_popcount()
     __builtin_popcountll()
 
     // Number of leading zeros
@@ -19,5 +19,5 @@
     __builtin_ctzll()
 
     // floor(log2(x))
-    int flog2(int x){ return 32-1-__builtin_clz(x); }
-    int flog2ll(ll x){ return 64-1-__builtin_clzll(x); }
+    int flog2(int x)  { return 32-1-__builtin_clz(x); }
+    int flog2ll(ll x) { return 64-1-__builtin_clzll(x); }
