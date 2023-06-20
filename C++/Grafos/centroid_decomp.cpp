cc7dc0f2bb3d5ecc7ff4cbcb1baef86bb8f9497f
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2023-06-20 02:03:56 -0300

    Update centroid_decomp.cpp

--- C++/Grafos/centroid_decomp.cpp
+++ C++/Grafos/centroid_decomp.cpp
@@ -1,39 +1,39 @@
 vector<int> g[N];
 int sz[N], rem[N];
 
-void dfs(vector<int>& path, int i, int l=-1, int d=0) {
+void dfs(vector<int>& path, int u, int d=0, int p=-1) {
     path.push_back(d);
-    for (int j : g[i]) if (j != l and !rem[j]) dfs(path, j, i, d+1);
+    for (int v : g[u]) if (v != p and !rem[v]) dfs(path, v, d+1, u);
 }
 
-int dfs_sz(int i, int l=-1) {
-    sz[i] = 1;
-    for (int j : g[i]) if (j != l and !rem[j]) sz[i] += dfs_sz(j, i);
-    return sz[i];
+int dfs_sz(int u, int p=-1) {
+    sz[u] = 1;
+    for (int v : g[u]) if (v != p and !rem[v]) sz[u] += dfs_sz(v, u);
+    return sz[u];
 }
 
-int centroid(int i, int l, int size) {
-    for (int j : g[i]) if (j != l and !rem[j] and sz[j] > size / 2)
-        return centroid(j, i, size);
-    return i;
+int centroid(int u, int p, int size) {
+    for (int v : g[u]) if (v != p and !rem[v] and sz[v] > size / 2)
+        return centroid(v, u, size);
+    return u;
 }
 
-ll decomp(int i, int k) {
-    int c = centroid(i, i, dfs_sz(i));
+ll decomp(int u, int k) {
+    int c = centroid(u, u, dfs_sz(i));
     rem[c] = true;
 
     ll ans = 0;
-    vector<int> cnt(sz[i]);
+    vector<int> cnt(sz[u]);
     cnt[0] = 1;
-    for (int j : g[c]) if (!rem[j]) {
+    for (int v : g[c]) if (!rem[v]) {
         vector<int> path;
-        dfs(path, j);
+        dfs(path, v);
         // d1 + d2 + 1 == k
-        for (int d : path) if (0 <= k-d-1 and k-d-1 < sz[i])
+        for (int d : path) if (0 <= k-d-1 and k-d-1 < sz[u])
             ans += cnt[k-d-1];
         for (int d : path) cnt[d+1]++;
     }
 
-    for (int j : g[c]) if (!rem[j]) ans += decomp(j, k);
+    for (int v : g[c]) if (!rem[v]) ans += decomp(j, k);
     return ans;
 }
