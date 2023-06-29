a3c4d9bac9803d6b0db15fa97c50e6fba9506fe7
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2023-06-29 20:24:28 -0300

    add methods for ordered set

--- C++/Misc/ordered_set.cpp
+++ C++/Misc/ordered_set.cpp
@@ -2,3 +2,6 @@
 using namespace __gnu_pbds; // or pb_ds;
 template<typename T, typename B = null_type>
 using ordered_set = tree<T, B, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+// order_of_key(k)  : Number of items strictly smaller than k
+// find_by_order(k) : K-th element in a set (counting from zero)
