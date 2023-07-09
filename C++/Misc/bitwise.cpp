062b08b5b3dc87bffee78c7f424f67abb628fc83
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2023-07-09 01:00:03 -0300

    bitwise

--- C++/Misc/bitwise.cpp
+++ C++/Misc/bitwise.cpp
@@ -1,23 +1,25 @@
 // Least significant bit (lsb)
-    int lsb(int x){ return x&-x; }
+    int lsb(int x) { return x&-x; }
+    int lsb(int x) { return __builtin_ctz(x); } // bit position
+// Most significant bit (msb)
+    int msb(int x) { return 32-1-__builtin_clz(x); } // bit position
 
 // Power of two
     bool isPowerOfTwo(int x){ return x && (!(x&(x-1))); }
 
+// floor(log2(x))
+int flog2(int x)  { return 32-1-__builtin_clz(x); }
+int flog2ll(ll x) { return 64-1-__builtin_clzll(x); }
+
 // Built-in functions
+// Number of bits 1
+__builtin_popcount()
+__builtin_popcountll()
 
-    // Number of bits 1
-    __builtin_popcount()
-    __builtin_popcountll()
+// Number of leading zeros
+__builtin_clz()
+__builtin_clzll()
 
-    // Number of leading zeros
-    __builtin_clz()
-    __builtin_clzll()
-
-    // Number of trailing zeros
-    __builtin_ctz()
-    __builtin_ctzll()
-
-    // floor(log2(x))
-    int flog2(int x)  { return 32-1-__builtin_clz(x); }
-    int flog2ll(ll x) { return 64-1-__builtin_clzll(x); }
+// Number of trailing zeros
+__builtin_ctz()
+__builtin_ctzll()
