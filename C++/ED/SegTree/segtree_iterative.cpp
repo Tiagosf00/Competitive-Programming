546939fd0d95775c8924f2aafa253a03256c016e
Tiago de Souza Fernandes <43066400+Tiagosf00@users.noreply.github.com>
2023-08-07 00:39:48 -0300

    Update segtree_iterative.cpp

--- C++/ED/SegTree/segtree_iterative.cpp
+++ C++/ED/SegTree/segtree_iterative.cpp
@@ -2,24 +2,24 @@ struct Segtree{
     int n; vector<int> t;
     Segtree(int n): n(n), t(2*n, 0) {}
 
-    int merge(int a, int b) { return max(a, b); }
+    int f(int a, int b) { return max(a, b); }
 
     void build(){
         for(int i=n-1; i>0; i--)
-            t[i] = merge(t[i<<1], t[i<<1|1]);
+            t[i] = f(t[i<<1], t[i<<1|1]);
     }
 
     int query(int l, int r) { // [l, r]
         int resl = -INF, resr = -INF;
         for(l+=n, r+=n+1; l<r; l>>=1, r>>=1) {
-            if(l&1) resl = merge(resl, t[l++]);
-            if(r&1) resr = merge(t[--r], resr);
+            if(l&1) resl = f(resl, t[l++]);
+            if(r&1) resr = f(t[--r], resr);
         }
-        return merge(resl, resr);
+        return f(resl, resr);
     }
 
     void update(int p, int value) {
         for(t[p+=n]=value; p >>= 1;)
-            t[p] = merge(t[p<<1], t[p<<1|1]);
+            t[p] = f(t[p<<1], t[p<<1|1]);
     }
 };
