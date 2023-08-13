a1405f1af337ab744b33a9bfea166ff397e6e117
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2023-08-13 20:57:12 -0300

    new hash template

--- C++/Strings/hash.cpp
+++ C++/Strings/hash.cpp
@@ -1,45 +1,27 @@
+// String Hash template
+// constructor(s) - O(|s|)
+// query(l, r) - returns the hash of the range [l,r] from left to right - O(1)
+// query_inv(l, r) from right to left - O(1)
+
+const ll MOD = 1e9+7;
+const ll P = 31;
 struct Hash {
-    vector<unordered_set<ll>> h;
-    vector<ll> mods = {
-        1000000009,1000000021,1000000033,
-        1000000087,1000000093,1000000097,
-        1000000103,1000000123,1000000181,
-        1000000207,1000000223,1000000241,
-        1000000271,1000000289,1000000297
-    };
-    ll p = 31;
-    int num;
-
-    Hash(int qt) {
-        srand(time(0));
-        num = qt;
-        h.assign(num, unordered_set<ll>());
-        random_shuffle(all(mods));
-    }
-
-    ll compute_hash(string const& s, ll p, ll m) {
-        ll res = 0, p_pow = 1;
-
-        for(char c : s) {
-            res = ( res + (c-'a'+1) * p_pow) % m;
-            p_pow = (p_pow * p) % m;
-        }
-        return res;
-    }
-
-    void add(string const& s) {
-        forn(i, num) {
-            ll value = compute_hash(s, p, mods[i]);
-            h[i].insert(value);
+    int n; string s;
+    vector<ll> h, hi, p;
+    Hash() {}
+    Hash(string s): s(s), n(s.size()), h(n), hi(n), p(n) {
+        for (int i=0;i<n;i++) p[i] = (P * (i ? p[i-1]:1)) % MOD;
+        for (int i=0;i<n;i++) {
+            h[i] = ((s[i] - 'a' + 1) * p[i] + (i ? h[i-1]:0)) % MOD;
+            hi[i] = ((s[i] - 'a' + 1) * p[n-i-1] + (i ? hi[i-1]:0)) % MOD;
         }
     }
-
-    bool query(string const& s) {
-        forn(i, num) {
-            ll val = compute_hash(s, p, mods[i]);
-            if(!h[i].count(val))
-                return false;
-        }
-        return true;
+    int query(int l, int r) {
+        ll hash = ((h[r] - (l ? h[l-1]:0)) * p[n-l-1] % MOD);
+        return hash < 0 ? hash + MOD : hash;
+    }
+    int query_inv(int l, int r) {
+        ll hash = ((hi[r] - (l ? hi[l-1]:0)) * p[r] % MOD);
+        return hash < 0 ? hash + MOD : hash;
     }
 };
