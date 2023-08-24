25a655627cfa97c9cbae2bfbcd597ee54bbaa240
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2023-08-23 23:57:38 -0300

    fix dijkstra spaces

--- C++/Grafos/dijkstra.cpp
+++ C++/Grafos/dijkstra.cpp
@@ -4,18 +4,18 @@ vector<bool> used(N);
 vector<ll> d(N, LLINF);
 priority_queue< pii, vector<pii>, greater<pii> > fila;
 
-void dijkstra(int k){
+void dijkstra(int k) {
     d[k] = 0;
     fila.push({0, k});
 
-    while(!fila.empty()){
+    while (!fila.empty()) {
         auto [w, u] = fila.top();
         fila.pop();
         if (used[u]) continue;
         used[u] = true;
 
-        for(auto [v, w]: g[u]){
-            if(d[v] > d[u] + w){
+        for (auto [v, w]: g[u]) {
+            if (d[v] > d[u] + w) {
                 d[v] = d[u] + w;
                 fila.push({d[v], v});   
             }
