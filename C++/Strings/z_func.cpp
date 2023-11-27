22227d1cd6d5f232b74c6cf92a85d8b05d2e9781
Alberto Duarte <albertotdneto@hotmail.com>
2023-11-26 22:20:45 -0300

    Muda variáveis da Z function

--- C++/Strings/z_func.cpp
+++ C++/Strings/z_func.cpp
@@ -1,11 +1,11 @@
 vector<int> Z(string s) {
     int n = s.size();
     vector<int> z(n);
-    int x = 0, y = 0;
+    int l = 0, r = 0;
     for (int i = 1; i < n; i++) {
-        z[i] = max(0, min(z[i - x], y - i + 1));
+        z[i] = max(0, min(z[i - l], r - i + 1));
         while (i + z[i] < n and s[z[i]] == s[i + z[i]]) {
-            x = i; y = i + z[i]; z[i]++;
+            l = i; r = i + z[i]; z[i]++;
         }
     }
     return z;
