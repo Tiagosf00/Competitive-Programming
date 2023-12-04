eee46812519ce6311c9a03225878ddf4ec7fcdf9
Tiago de Souza Fernandes <43066400+Tiagosf00@users.noreply.github.com>
2023-12-04 09:07:51 -0300

    I hate structs

--- C++/Strings/trie.cpp
+++ C++/Strings/trie.cpp
@@ -1,34 +1,31 @@
-struct Trie{
+int trie[N][26];
+bool finish[N];
+int nxt = 1, len = 0;
 
-    int trie[MAX][26];
-    bool finish[MAX];
-    int nxt = 1, len = 0;
-
-    void add(string s){
-        int node = 0;
-        for(auto c: s){
-            if(trie[node][c-'a'] == 0)
-                node = trie[node][c-'a'] = nxt++;
-            else
-                node = trie[node][c-'a'];
-        }
-        if(!finish[node]){
-            finish[node] = true;
-            len++;
-        }
+void add(string s){
+    int node = 0;
+    for(auto c: s){
+        if(trie[node][c-'a'] == 0)
+            node = trie[node][c-'a'] = nxt++;
+        else
+            node = trie[node][c-'a'];
     }
-
-    bool find(string s, bool remove=false){
-        int node = 0;
-        for(auto c: s)
-            if(trie[node][c-'a'] == 0)
-                return false;
-            else
-                node = trie[node][c-'a'];
-        if(remove and finish[node]){
-            finish[node]=false;
-            len--;
-        }
-        return finish[node];
+    if(!finish[node]){
+        finish[node] = true;
+        len++;
     }
-};
+}
+
+bool find(string s, bool remove=false){
+    int node = 0;
+    for(auto c: s) {   
+        if(trie[node][c-'a'] == 0)
+            return false;
+        else
+            node = trie[node][c-'a'];
+    }
+    if(remove and finish[node]){
+        finish[node]=false;
+        len--;
+    return finish[node];
+}
