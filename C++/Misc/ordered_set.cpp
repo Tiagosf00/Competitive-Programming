e388fe3b31977e23e6bf245a60584cb39fbdbd08
Tiagosf00 <tiago.fernandes@incognia.com>
2023-12-28 13:02:29 -0300

    ordered_set and tabs

--- C++/Misc/ordered_set.cpp
+++ C++/Misc/ordered_set.cpp
@@ -1,4 +1,8 @@
 #include <bits/extc++.h>
+// or
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
 using namespace __gnu_pbds; // or pb_ds;
 template<typename T, typename B = null_type>
 using ordered_set = tree<T, B, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
